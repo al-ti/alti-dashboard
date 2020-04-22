73a1a9c7dd1e80430c1143e25b6610f1b4d4ea77
Oleksii Tymchenko <oleksii.tymchenko@ventus.ag>
2020-04-22 17:14:01 +0300

    Test ventus dashboard

--- ventusdashboard/ventus_panel/templates/ventus_panel/views.py
+++ ventusdashboard/ventus_panel/templates/ventus_panel/views.py
@@ -4,7 +4,7 @@ from horizon import views
 class IndexView(tabs.TabbedTableView):
     tab_group_class = mydashboard_tabs.MypanelTabs
     # A very simple class-based view...
-    template_name = 'ventus_dashboard/ventus_panel/index.html'
+    template_name = 'ventusdashboard/ventus_panel/index.html'
 
     def get_data(self, request, context, *args, **kwargs):
         # Add data to the context here...
