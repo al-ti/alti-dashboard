4a7315a1fe919a8336e29a887c86984f703ad737
Oleksii Tymchenko <oleksii.tymchenko@ventus.ag>
2020-04-22 20:00:04 +0300

    Test ventus dashboard

--- ventusdashboard/ventus_panel/views.py
+++ ventusdashboard/ventus_panel/views.py
@@ -1,9 +1,11 @@
-from horizon import views
+from horizon import tabs
+
+from openstack_dashboard.dashboards.ventusdashboard.ventus_panel \
+    import tabs as ventusdashboard
 
 
 class IndexView(tabs.TabbedTableView):
-    tab_group_class = mydashboard_tabs.MypanelTabs
-    # A very simple class-based view...
+    tab_group_class = ventusdashboard.MypanelTabs
     template_name = 'ventusdashboard/ventus_panel/index.html'
 
     def get_data(self, request, context, *args, **kwargs):
